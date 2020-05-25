a9a8538699a54f016f8a94cb690675fe39d71ceb
J08nY <johny@neuromancer.sk>
2020-05-25 19:00:54 +0200

    Prototype.

--- smartkar/__init__.py
+++ smartkar/__init__.py
@@ -1,11 +1,13 @@
 import os
 
-from flask import Flask, render_template, request
+import requests
+from flask import Flask, render_template, request, abort
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, validators, FloatField
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["NCZI_TOKEN"] = os.environ.get("NCZI_TOKEN")
 
 csrf = CSRFProtect(app)
 
@@ -14,24 +16,59 @@ try:
 except OSError:
   pass
 
+
 class LocationForm(FlaskForm):
-  token = StringField("token", [validators.required(), validators.Length(min=64,max=64)])
+  token = StringField("token", [validators.required(), validators.Length(min=32, max=32)])
   lat = FloatField("lat", [validators.required()])
   lng = FloatField("lng", [validators.required()])
-  alt = FloatField("alt", [validators.required()])
-  accuracy = FloatField("accuracy", [validators.required()])
 
-
-@app.route("/")
-def index():
-  return render_template("index.html.jinja2")
-
-
-@app.route("/<string:token>", methods=["GET", "POST"])
+@app.route("/geo/<string:token>", methods=["GET", "POST"])
 def locate(token):
+  if len(token) != 32:
+    abort(404)
+
   form = LocationForm()
   if request.method == "GET":
+    form.token.data = token
     return render_template("locate.html.jinja2", token=token, form=form)
   elif form.validate_on_submit():
-    pass
-
+    if app.env == "production":
+      endpoint = "https://mojeezdravie.nczisk.sk/api/v1/sq/quarantine-geo-location"
+    else:
+      endpoint = "https://t.mojeezdravie.sk/api/v1/sq/quarantine-geo-location"
+    data = {
+      "vOneTimeToken": form.token.data,
+      "nQuarantineAddressLatitude": form.lat.data,
+      "nQuarantineAddressLongitude": form.lng.data
+    }
+    headers = {
+      "Authorization": "Bearer " + app.config["NCZI_TOKEN"]
+    }
+    resp = requests.post(endpoint, json=data, headers=headers)
+    ok = True
+    message = ""
+    if resp.status_code == 200:
+      message = "Poloha domácej izolácie bola úspešne aktualizovaná."
+    elif resp.status_code == 202:
+      ok = False
+      try:
+        resp_data = resp.json()
+        message = resp_data["ErrorText"]
+      except Exception:
+        message = "Chyba. Poloha domácej izolácie už bola raz aktualizovaná."
+    elif resp.status_code == 400:
+      ok = False
+      message = "Chyba. Bad request."
+    elif resp.status_code == 404:
+      ok = False
+      message = "Chyba. Nesprávny token."
+    elif resp.status_code == 401:
+      ok = False
+      message = "Chyba. Unauthorized."
+    elif resp.status_code == 403:
+      ok = False
+      message = "Chyba. Forbidden."
+    return render_template("located.html.jinja2", lat=form.lat.data, lng=form.lng.data,
+                           message=message, ok=ok)
+  else:
+    print(form.data)
--- smartkar/static/locate.js
+++ smartkar/static/locate.js
@@ -1,17 +1,55 @@
-function load() {
+let best_position = null;
+
+function init_map() {
+  let map = new google.maps.Map($("#map")[0], {
+    center: {lat: 48.7071075, lng: 19.7599799},
+    zoom: 8,
+    streetViewControl: false,
+    fullscreenControl: false,
+    mapTypeId: 'hybrid'
+  });
+  let marker = new google.maps.Marker({
+    position: {lat: 48.7071075, lng: 19.7599799}
+  });
+
   function success_cb(position) {
-    console.log(position);
+    $("#overlay").hide();
+    $("#send-button").prop("disabled", false);
+    if (best_position === null || best_position.accuracy > position.coords.accuracy) {
+      best_position = {lat: position.coords.latitude, lng: position.coords.longitude, accuracy: position.coords.accuracy};
+    }
+    let geo = {lat: position.coords.latitude, lng: position.coords.longitude};
+    map.setCenter(geo);
+    marker.setMap(map);
+    marker.setPosition(geo);
+    if (map.getZoom() < 17) {
+      map.setZoom(17);
+    }
   }
 
   function error_cb(error) {
     console.log(error);
+    if (best_position === null) {
+      if (error.code === 1) {
+        $("#overlay div").hide();
+        $("#overlay .allow").show();
+      } else if (error.code === 2) {
+        $("#overlay div").hide();
+        $("#overlay .unavailable").show();
+      }
+    }
   }
 
   if (!navigator.geolocation) {
-
   } else {
-    navigator.geolocation.getCurrentPosition(success_cb, error_cb, {"enableHighAccuracy": true});
+    navigator.geolocation.watchPosition(success_cb, error_cb, {"enableHighAccuracy": true});
   }
 }
 
-$(document).ready(load)
+function submit() {
+  if (best_position !== null) {
+    $("#lat").val(best_position.lat);
+    $("#lng").val(best_position.lng);
+    $("#form").submit();
+  }
+}
